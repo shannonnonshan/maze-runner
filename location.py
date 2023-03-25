f28de8fb2e132993eada7b7f859c1443447753ab
Huy Hoang Lam Nguyen <huyyhoangg150203@gmail.com>
2023-03-26 00:20:59 +0700

    fix

--- location.py
+++ location.py
@@ -8,9 +8,6 @@ class Location:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.g_score = 1
-        self.f_score = 1
-        self.parent = None
 
     def __repr__(self):
         return f"({self.x}, {self.y})"
@@ -26,7 +23,3 @@ class Location:
     @staticmethod
     def sort(locations):
         return sorted(locations, key=lambda loc: (loc.x, loc.y))
-
-    @staticmethod
-    def heuristic(a, b) -> float:
-        return math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2)
